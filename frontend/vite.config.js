b77ad423045b3ff96ba6e47b2189248d824306be
Nomena Vahatriniaina <119030942+Nomena-1918@users.noreply.github.com>
2025-09-28 18:59:56 +0400

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,27 +1,27 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [vue()],
-  base: '/',
-server: {
-  proxy: {
-    '/ecran-travail': {
-        target: 'http://localhost:8080/gestion_projet_cinema',
-        changeOrigin: true,
-        secure: false
-      },
-    '/gestion_projet_cinema': {
-      target: 'http://localhost:8080/gestion_projet_cinema',
-      changeOrigin: true,
-      rewrite: (path) => path 
+    plugins: [vue()],
+    base: '/gestion_projet_cinema',
+    server: {
+        proxy: {
+            '/ecran-travail': {
+                target: 'http://localhost:8080/gestion_projet_cinema',
+                changeOrigin: true,
+                secure: false
+            },
+            '/gestion_projet_cinema': {
+                target: 'http://localhost:8080/gestion_projet_cinema',
+                changeOrigin: true,
+                rewrite: (path) => path
+            }
+        }
+    },
+    build: {
+        outDir: '../backend/src/main/resources/static',
+        emptyOutDir: true,
+        assetsDir: 'assets',
+        sourcemap: false,
     }
-  }
-},
-  build: {
-    outDir: '../backend/src/main/resources/static',
-    emptyOutDir: true,
-    assetsDir: 'assets',
-    sourcemap: false,
-  }
 })
